382e9ec07e0bfa8f9bb74682e3fe4923fb7b91e9
mussabaheen <mussabaheen@gmail.com>
2017-08-26 14:52:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mussabaheenmalik/myui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mussabaheenmalik.myui;
+
+import android.graphics.Color;
+import android.graphics.drawable.StateListDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+    }
+
+    public void button(View view) {
+        Toast.makeText(this, "IMAGE BUTTON CLICKED ! ", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
